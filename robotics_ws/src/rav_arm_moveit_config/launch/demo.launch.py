85358b6821da72753842a8550c253ab73166cc9e
Nathan Conconi <nathanconconi24@gmail.com>
2026-04-25 00:49:12 -0300

    add pacote moveit com .urdf para criacao de poses home, open, closed, attack no moveit com setup assistant

--- robotics_ws/src/rav_arm_moveit_config/launch/demo.launch.py
+++ robotics_ws/src/rav_arm_moveit_config/launch/demo.launch.py
@@ -1,7 +1,7 @@
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_setup_assistant_launch
+from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("rav_arm", package_name="rav_arm_moveit_config").to_moveit_configs()
-    return generate_setup_assistant_launch(moveit_config)
+    return generate_demo_launch(moveit_config)
--- robotics_ws/src/rav_arm_moveit_config/launch/warehouse_db.launch.py
+++ robotics_ws/src/rav_arm_moveit_config/launch/warehouse_db.launch.py
@@ -1,7 +1,7 @@
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_setup_assistant_launch
+from moveit_configs_utils.launches import generate_warehouse_db_launch
 
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("rav_arm", package_name="rav_arm_moveit_config").to_moveit_configs()
-    return generate_setup_assistant_launch(moveit_config)
+    return generate_warehouse_db_launch(moveit_config)
